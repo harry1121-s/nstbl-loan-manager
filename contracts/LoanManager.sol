fb770ae0693ce7677c64ff690821892558bfe711
Harshit Singhal <82471455+harry1121-s@users.noreply.github.com>
2023-08-23 13:11:15 +0530

    usdt pool/code-added

--- contracts/LoanManager.sol
+++ contracts/LoanManager.sol
@@ -8,33 +8,65 @@ contract loanManager is loanManagerStorage{
 
     using SafeERC20 for IERC20;
 
-    // modifier authorizedCaller{
-    //     require(msg.sender == nstblHub, "Loan Manager: unAuth");
-    //     _;
-    // }
+    modifier authorizedCaller{
+        require(msg.sender == nstblHub, "Loan Manager: unAuth");
+        _;
+    }
 
-    constructor(address _nstblHub, address _mapleCashPool, address _USDCToken){
+    constructor(
+        address _nstblHub, 
+        address _mapleUSDCPool, 
+        address _mapleUSDTPool, 
+        address _USDCAsset, 
+        address _USDTAsset){
         nstblHub = _nstblHub;
-        mapleUSDCPool = IPool(_mapleCashPool);
-        usdcToken = IERC20(_USDCToken);
+        mapleUSDCPool = IPool(_mapleUSDCPool);
+        mapleUSDTPool = IPool(_mapleUSDTPool);
+        usdcAsset = IERC20(_USDCAsset);
+        usdtAsset = IERC20(_USDTAsset);
 
     }
 
-    function investUSDCMapleCash(uint256 _assets)public{
+    function investUSDCMapleCash(uint256 _assets)public authorizedCaller{
 
-        usdcToken.safeTransferFrom(msg.sender, address(this), _assets);
+        usdcAsset.safeTransferFrom(msg.sender, address(this), _assets);
         usdcDeposited += _assets;
-        usdcToken.approve(address(mapleUSDCPool), _assets);
-        mapleUSDCPool.deposit(_assets, address(this));
-        usdcSharesReceived += mapleUSDCPool.balanceOf(address(this));
-
+        usdcAsset.approve(address(mapleUSDCPool), _assets);
+        usdcSharesReceived += mapleUSDCPool.deposit(_assets, address(this));
     }
 
-    function requestRedeemUSDCMapleCash(uint256 _shares)public returns(uint256){
+    function requestRedeemUSDCMapleCash(uint256 _shares)public authorizedCaller returns(uint256){
         require(mapleUSDCPool.balanceOf(address(this)) >= _shares, "Insufficient amount");
-        usdcSharesRequestedForRedeem += _shares;
+        usdcSharesRequestedForRedeem = _shares;
         return mapleUSDCPool.requestRedeem(_shares, address(this));
     }
 
+    function redeemUSDCMapleCash()public authorizedCaller{
+        uint256 _shares = usdcSharesRequestedForRedeem;
+        usdcRedeemed += mapleUSDCPool.redeem(_shares, nstblHub, address(this));
+
+    }
+
+    function investUSDTMapleCash(uint256 _assets)public authorizedCaller{
+
+        usdtAsset.safeTransferFrom(msg.sender, address(this), _assets);
+        usdtDeposited += _assets;
+        usdtAsset.approve(address(mapleUSDTPool), _assets);
+        usdtSharesReceived += mapleUSDTPool.deposit(_assets, address(this));
+
+    }
+
+    function requestRedeemUSDTMapleCash(uint256 _shares)public authorizedCaller returns(uint256){
+        require(mapleUSDTPool.balanceOf(address(this)) >= _shares, "Insufficient amount");
+        usdtSharesRequestedForRedeem = _shares;
+        return mapleUSDTPool.requestRedeem(_shares, address(this));
+    }
+
+    function redeemUSDTMapleCash()public authorizedCaller{
+        uint256 _shares = usdtSharesRequestedForRedeem;
+        usdcRedeemed += mapleUSDTPool.redeem(_shares, nstblHub, address(this));
+
+
+    }
 
 }
--- contracts/LoanManagerStorage.sol
+++ contracts/LoanManagerStorage.sol
@@ -8,13 +8,26 @@ contract loanManagerStorage{
 
     address public nstblHub;
 
-    IERC20 public usdcToken;
+    IERC20 public usdcAsset;
+    IERC20 public usdtAsset;
 
     uint256 public usdcDeposited;
+
+    uint256 public usdcRedeemed;
 
     uint256 public usdcSharesReceived;
 
     uint256 public usdcSharesRequestedForRedeem;
 
+    uint256 public usdtDeposited;
+
+    uint256 public usdtRedeemed;
+
+    uint256 public usdtSharesReceived;
+
+    uint256 public usdtSharesRequestedForRedeem;
+
     IPool public mapleUSDCPool;
+
+    IPool public mapleUSDTPool;
 }
