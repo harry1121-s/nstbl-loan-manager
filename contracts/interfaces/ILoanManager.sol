008c5256ffc28a0880d369415811ffbaeb562e05
MrAngad <angad.97@protonmail.com>
2023-08-28 11:44:03 +0530

    refactored a bit / added ILoanManager

--- contracts/interfaces/ILoanManager.sol
+++ contracts/interfaces/ILoanManager.sol
@@ -2,5 +2,34 @@
 pragma solidity ^0.8.19;
 
 interface ILoanManager {
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
 
+    event Deposit(address indexed asset, uint256 amount);
+
+    event RequestRedeem(address indexed asset, uint256 shares);
+
+    event Redeem(address indexed asset, uint256 shares);
+
+    /*//////////////////////////////////////////////////////////////
+                                 LP Functions
+    //////////////////////////////////////////////////////////////*/
+
+    function deposit(address asset, uint256 amount) external;
+
+    function requestRedeem(address asset, uint256 shares) external;
+
+    function redeem(address asset, uint256 shares) external;
+
+    /*//////////////////////////////////////////////////////////////
+                                 VIEWS
+    //////////////////////////////////////////////////////////////*/
+
+    function previewDeposit(address asset, uint256 amount) external view returns (uint256);
+    
+    function previewRedeem(address asset, uint256 shares) external view returns (uint256);
+
+    function unrealizedLosses(address asset, uint256 shares) external view returns (uint256);
+    
 }
